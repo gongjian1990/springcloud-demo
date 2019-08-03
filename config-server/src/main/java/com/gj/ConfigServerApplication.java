f5a1e3e9da078b1da14150156d791b0118e28d14
gongjian <602192564@qq.com>
2019-08-03 10:53:34 +0800

    提交

--- config-server/src/main/java/com/gj/ConfigServerApplication.java
+++ config-server/src/main/java/com/gj/ConfigServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class SpringcloudConfigServerApplication {
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudConfigServerApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 
 }
